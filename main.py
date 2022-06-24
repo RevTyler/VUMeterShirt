a75a295e830f88f2d19e1ea26c38bfbb4c84c1cb
Tyler <rev.tylerreid@gmail.com>
2022-06-23 22:07:43 -0700

    no message

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import neopixel
 pixels = neopixel.NeoPixel(board.D18, 5, brightness=0.5, pixel_order=neopixel.GRB)
 
 pixels[0] = (255, 0, 0)
-pixels[0] = (0, 255, 0)
-pixels[0] = (0, 0, 255)
+pixels[1] = (0, 255, 0)
+pixels[2] = (0, 0, 255)
