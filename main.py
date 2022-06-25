619e8f687aaf9e8d0fa0ddb0cd8809cb5e99b552
Tyler <rev.tylerreid@gmail.com>
2022-06-24 19:53:18 -0700

    no message

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import time
 import board
 import neopixel
 
-pixels = neopixel.NeoPixel(board.D18, 5)
+pixels = neopixel.NeoPixel(board.D18, 5, brightness=0.5, pixel_order=neopixel.GRB)
 
-pixels.fill((0, 128, 0))
+pixels[0] = (200, 0, 0)
+time.sleep(0.5)
+pixels[0] = (0, 200, 0)
+time.sleep(0.5)
+pixels[0] = (0, 0, 200)
+time.sleep(0.5)
+pixels[0] = (200, 200, 0)
+time.sleep(0.25)
+pixels[0] = (200, 0, 0)
+time.sleep(0.25)
+pixels[0] = (200, 200, 0)
+time.sleep(0.5)
 pixels.show()
