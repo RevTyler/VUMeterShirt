027c5a2a3ff3dc98ff115b030383f2c414694e8e
Tyler <rev.tylerreid@gmail.com>
2022-06-24 17:45:22 -0700

    no message

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-#import board
-#import neopixel
+import board
+import neopixel
 
-#pixels = neopixel.NeoPixel(board.D18, 5, brightness=0.5, pixel_order=neopixel.GRB)
+pixels = neopixel.NeoPixel(board.D18, 5, brightness=0.5, pixel_order=neopixel.GRB)
 
-#pixels[0] = (255, 0, 0)
-#pixels[1] = (0, 255, 0)
-#pixels[2] = (0, 0, 255)
-#pixels.show()
-import RPi.GPIO as GPIO
-
-GPIO.setmode(GPIO.BOARD)
-
-GPIO.setup(18, GPIO.OUT)
-GPIO.output(18, 1)
+pixels[0] = (255, 0, 0)
+pixels[1] = (0, 255, 0)
+pixels[2] = (0, 0, 255)
+pixels.show()
